49caced8bcde85d3ebfa93ce381fc3f72e972188
Your Name <user@example.com>
2026-01-26 05:38:30 +0300

    pravki

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -3,6 +3,7 @@ URL маршруты для главного приложения.
 """
 
 from django.urls import path
+from django.views.generic import TemplateView
 from apps.main.views import HomeView, AboutView
 
 app_name = 'main'
@@ -10,5 +11,6 @@ app_name = 'main'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
+    path('privacy/', TemplateView.as_view(template_name='main/privacy.html'), name='privacy'),
 ]
 
